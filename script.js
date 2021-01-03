f3a16a4302ad7b3d75f2de35b9a7348d592f87a2
Kat Buchanan <katrbuch@gmail.com>
2021-01-03 15:11:19 -0600

    Tweaked layout, edited linked scripts and meta tags.

---
+++ script.js
@@ -0,0 +1,4 @@
+/* These plugins will help easily save high scores to local storage.
+<script src="/path/to/jquery.min.js"></script>
+<script src="/path/to/savestorage.js"></script>
+<!--$('#playerName').saveStorage();-->
