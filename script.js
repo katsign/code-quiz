20d1a62d85200b1c70532a9a2a93952d29ef03e6
Kat Buchanan <katrbuch@gmail.com>
2021-01-04 12:26:17 -0600

    Responding to console error.

--- script.js
+++
@@ -1,4 +0,0 @@
-/* These plugins will help easily save high scores to local storage.
-<script src="/path/to/jquery.min.js"></script>
-<script src="/path/to/savestorage.js"></script>
-<!--$('#playerName').saveStorage();-->
